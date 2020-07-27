211e83d2e764716c9ea5c084307732570470e432
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-07-27 15:00:18 -0500

    added modal for 'no' button

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,45 +1,104 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import "../assets/css/about.css";
 import How from "../components/How";
-import VotePlan from "../components/VotePlan"
+import VotePlan from "../components/VotePlan";
+import Modal from 'react-modal';
 
-class About extends Component {
+function About () {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            showComponent: false,
-            showButton: true
-        };
-        this._onButtonClick = this._onButtonClick.bind(this);
-    }
+    var subtitle;
+    const customStyles = {
+        content : {
+          top                   : '50%',
+          left                  : '50%',
+          right                 : 'auto',
+          bottom                : 'auto',
+          marginRight           : '-50%',
+          transform             : 'translate(-50%, -50%)',
+          backgroundColor : '#004789', 
+          borderRadius: '15px', 
+          color: 'white', 
+          textAlign: 'center',
+        }
+      };
 
-    _onButtonClick() {
-        console.log("clicked")
-        this.setState({
-            showComponent: true,
-            showButton: false
-        });
+    const [modalIsOpen,setIsOpen] = useState(false);
+    const [component, showComponent] = useState(false); 
+    const [button, showButton] = useState(true); 
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         showComponent: false,
+    //         showButton: true,
+    //         showModalYes: false,
+    //         showModalNo: false,
+    //         close: false
+    //     };
+    //     this._onButtonClickYes = this._onButtonClickYes.bind(this);
+    //     this._onButtonClickNo = this._onButtonClickNo.bind(this);
+    //     this.closeModal = this.closeModal.bind(this)
 
-    }
+    // }
+    function openModal() {
+        setIsOpen(true);
+      }
+     
+      function closeModal(){
+        setIsOpen(false);
+      }
+     
+    // _onButtonClickYes() {
+    //     console.log("yes")
+    //     this.setState({
+    //         showButton: false,
+    //         showModalYes: true
+    //     });
 
-    render() {
+    // }
+    
+    // _onButtonClickNo() {
+    //     console.log("no")
+    //     this.setState({
+    //         showButton: false,
+    //         showModalNo: true
+    //     });
+
+    // }
+
         return (
             <div class="container">
                 <div class="jumbotron">
                     <div>
-                        {this.state.showComponent ?
+                        {component ?
                             <How /> : <VotePlan />
                         }
-                        {this.state.showButton ?
-                        <div style = {{textAlign: "center"}}>
-                            <button class="btn btn-primary" onClick={this._onButtonClick}>Let's go</button> 
-                        </div>      : null
+                        {button ?
+                            <div className="row">
+                                <div className="col-sm-6" style={{ textAlign: "center", position: "relative", left: "200px" }}>
+                                    <button class="btn btn-primary" onClick={showComponent}>yes</button>
+                                </div>
+                                <Modal
+                                    isOpen={modalIsOpen}
+                                    // onAfterOpen={afterOpenModal}
+                                    onRequestClose={closeModal}
+                                    style={customStyles}
+                                    contentLabel="Example Modal"
+                                >
+                                    <i id = "x" class="fa fa-times" onClick={closeModal}></i>
+                                    {/* <div>No problem! </div> */}
+                                    <div style ={{ marginBottom: "50px"}}>
+                                    <h3>No problem!</h3>
+                                     <a style ={{color: "white !important"}} href = "https://turbovote.org/" target = "_blank">Click here to register - then come on back to fill out your Voting Plan</a>
+                                     </div>
+                                </Modal>
+                                <div className="col-sm-6" style={{ textAlign: "center", position: "relative", right: "200px" }}>
+                                    <button class="btn btn-primary" onClick={openModal}>no</button>
+                                </div>
+                            </div> : null
                         }
                     </div>
                 </div>
             </div>
         )
-    }
 }
 export default About;
--- client/src/components/VotePlan.js
+++ client/src/components/VotePlan.js
@@ -4,13 +4,17 @@ function VotePlan() {
     return (
         <div id="begin" style={{ marginBottom: "50px", textAlign: "center" }}>
             <h1>
-                VOTING PLAN
+                VOTING PLAN 
             </h1>
             {/* <div>
                 <a class="twitter-share-button"
                     href="https://twitter.com/intent/tweet" target="_blank">
                     Tweet</a>
             </div> */}
+
+            <h3>
+                Are you registered to vote?
+            </h3>
 
         </div>
     )
