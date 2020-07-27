3531441428d13f01bdaaaf743f53c71b74cf6415
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-07-27 15:33:51 -0500

    adding build folder

---
+++ client/build/precache-manifest.8f0315e87ae2730e2ad810e542d51f4b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b840f7cedf66a27b0f8a6600d943c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391f8f19dc01921e0f00",
+    "url": "/static/css/2.25a01bf0.chunk.css"
+  },
+  {
+    "revision": "79ed70b2fac75379f892",
+    "url": "/static/css/main.7d31f3c3.chunk.css"
+  },
+  {
+    "revision": "391f8f19dc01921e0f00",
+    "url": "/static/js/2.ff86489e.chunk.js"
+  },
+  {
+    "revision": "0808c75b50af43a39fe893124e2f5e72",
+    "url": "/static/js/2.ff86489e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ed70b2fac75379f892",
+    "url": "/static/js/main.df560326.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "d803d237bc9d24e64bfeeb3878457a30",
+    "url": "/static/media/vote.d803d237.jpg"
+  }
+]);
