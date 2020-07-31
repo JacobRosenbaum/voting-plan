69f0e5ba9360b37300ec32982634c39a09a95909
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-07-31 10:55:45 -0500

    adding .env file

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/votingPlanDB"), { useNewUrlParser: true };
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
