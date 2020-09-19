2252b90d806ad897622c5fc363a4fbd9d9d5e690
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-09-19 09:36:46 -0500

    added embedded video

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Main from "../src/pages/Main";
 import "../src/assets/css/app.css";
 import oneSticker from "../src/assets/images/oneSticker.png";
 import twoStickers from "../src/assets/images/twoStickers.png";
 import i from "../src/assets/images/exclamation.png";
-import letsDoSomething from "../src/assets/images/letsDoSomething.png"
+import letsDoSomething from "../src/assets/images/letsDoSomething.png";
+import Modal from 'react-modal';
+
 
 
 function App() {
+
+    const [ModalIsOpen, setModalIsOpen] = useState(false);
+
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#004789',
+            borderRadius: '15px',
+            color: 'white',
+            textAlign: 'center',
+            border: '5px #EF3D55 solid',
+            zIndex: 205
+        }
+    };
+    function openModal() {
+        setModalIsOpen(true);
+    }
+
+    function closeModal() {
+        setModalIsOpen(false);
+    }
     return (
         <div>
             <div class="animate__animated animate__fadeIn">
@@ -16,7 +44,27 @@ function App() {
             <img id = "twoStickers" src={twoStickers}/> */}
             </div>
             <footer style={{ textAlign: 'center' }}>
-                <div style={{ textAlign: 'center' }}>Let's DO Something</div>
+                <div style={{ textAlign: 'center' }}
+                onClick={openModal}
+                id = "about"
+            
+                >
+                About Us
+                    </div>
+                <Modal
+                        isOpen={ModalIsOpen}
+                        // onAfterOpen={afterOpenModal}
+                        onRequestClose={closeModal}
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                    >
+                        <i id="x" class="fa fa-times" onClick={closeModal}></i>
+                        <iframe src="https://player.vimeo.com/video/453484807" id = "iFrame"
+                        width="640" height="360" frameborder="0" 
+                        allow="autoplay; fullscreen" allowfullscreen></iframe>
+
+                        
+                    </Modal>
                 <div className = "red end">
                 {/* <span
   class="fb-share-button" 
